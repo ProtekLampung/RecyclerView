1e87baa708ebae27bc65aaa54f3401e5e80acbae
doofensmith <wahyuwijanarko3998@gmail.com>
2021-11-18 17:31:58 +0700

    Contact Detail

---
+++ app/src/main/java/com/protek/recyclerview/DetailContact.java
@@ -0,0 +1,14 @@
+package com.protek.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailContact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_contact);
+    }
+}
